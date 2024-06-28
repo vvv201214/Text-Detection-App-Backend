deb6cf2ef4cb8b5ceaaec49e166ce54221a583a0
vijay <vvv201214@gmail.com>
2024-06-28 11:04:39 +0530

    server loaction change

---
+++ server.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const dotenv = require('dotenv');
+const path = require("path");
+const {googleApiLogin} = require('./src/services/ocr')
+
+dotenv.config({path: path.resolve(__dirname, './config.env')});
+
+const app = require('./src/app');
+
+
+const dB = process.env.DEV_DB
+const PORT = process.env.PORT || 8080;
+
+mongoose.connect(dB).then(()=>{
+    console.log('Database Connected!');
+});
+
+googleApiLogin().then(()=>{});
+
+const server = app.listen(PORT, ()=>{
+    console.log(`Server running at ${PORT}`)
+});
